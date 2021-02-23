60ba73763ba795e5e97c7d4cee3069be39ad296c
Dean <yliu224@dons.usfca.edu>
2021-02-23 00:45:00 -0800

    Add binary search template and AC lc159

---
+++ dean/solution/lc159/Solution.java
@@ -0,0 +1,28 @@
+package dean.solution.lc159;
+
+public class Solution{
+    /**
+     * @param nums: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums[0]<nums[nums.length-1]){
+            return nums[0];
+        }
+        int left = 0,right = nums.length-1;
+        while(left+1<right){
+            int mid = (left+right)/2;
+
+            if(nums[mid]>nums[left]){
+                left = mid;
+            } else{
+                right = mid;
+            }
+        }
+
+        return Math.min(nums[left],nums[right]);
+    }
+}
