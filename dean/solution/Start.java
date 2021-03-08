2f82aed53032652f2cfe0d3243a208c48f9183e5
Dean <yliu224@dons.usfca.edu>
2021-03-08 00:13:45 -0800

    AC 1219

--- dean/solution/Start.java
+++ dean/solution/Start.java
@@ -1,11 +1,11 @@
 package dean.solution;
 
-import dean.solution.lc1375_m.*;
+import dean.solution.lc1219_e.*;
 
 public class Start {
     public static void main(String[] args){
         Solution s = new Solution();
 
-        System.out.println(s.kDistinctCharacters("bkacsrgjkynenkwbfaeblmbjfjshojryawvsuftmqftkjqnsjfhvbsddwiwmifhphftkckkedzotkbiplqjsgdyqgjnclwjqxbbc",2));
+        System.out.println(s.findRadius(new int[]{1,100},new int[]{1,2,4,90}));
     }
 }
---
+++ dean/solution/lc1219_e/Solution.java
@@ -0,0 +1,26 @@
+package dean.solution.lc1219_e;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int j=0;
+        int minRadius = Integer.MIN_VALUE;
+        for(int i=0;i<houses.length;i++){
+            while(j<heaters.length-1 && houses[i]>=heaters[j+1]){
+                j++;
+            }
+            int r1 = Math.abs(houses[i]-heaters[j]);
+            //不一定有r2
+            int r2 = Integer.MAX_VALUE;
+            if(j+1<=heaters.length-1){
+                r2 = Math.abs(houses[i]-heaters[j+1]);
+            }
+
+            minRadius = Math.max(minRadius,Math.min(r1,r2));
+        }
+        return minRadius;
+    }
+}
