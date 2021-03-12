18dc138402c22466a89b62df583f8c2091c9b1d6
Dean <yliu224@dons.usfca.edu>
2021-03-11 23:39:34 -0800

    AC 386

--- dean/solution/Start.java
+++ dean/solution/Start.java
@@ -1,11 +1,11 @@
 package dean.solution;
 
-import dean.solution.two_pointer.lc406_h.*;
+import dean.solution.two_pointer.lc386_e.*;
 
 public class Start {
     public static void main(String[] args){
         Solution s = new Solution();
 
-        System.out.println(s.subarraySumII(new int[]{1,2,3,4}, 0, 0));
+        System.out.println(s.lengthOfLongestSubstringKDistinct("eceba", 3));
     }
 }
---
+++ dean/solution/two_pointer/lc386_e/Solution.java
@@ -0,0 +1,56 @@
+package dean.solution.two_pointer.lc386_e;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    Map<Character,Integer> wordCount = new HashMap<>();
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        // write your code here
+        if(s.isEmpty()) return 0;
+        int maxLen = Integer.MIN_VALUE;
+        int j=1;
+        wordCount.put(s.charAt(0),1);
+        for(int i=0;i<s.length();i++){
+            while(j<s.length() && isValidSubStr(k)){
+                maxLen = Math.max(maxLen,j-i);
+                this.add(s.charAt(j));
+                j++;
+            }
+
+            if(isValidSubStr(k)){
+                maxLen = Math.max(maxLen,j-i);
+            }
+            this.delete(s.charAt(i));
+        }
+        return maxLen==Integer.MIN_VALUE?0:maxLen;
+    }
+
+    private void add(char c){
+        if(wordCount.containsKey(c)){
+            wordCount.put(c,wordCount.get(c)+1);
+        } else {
+            wordCount.put(c,1);
+        }
+    }
+
+    private void delete(char c){
+        if(wordCount.containsKey(c)){
+            if(wordCount.get(c)==1){
+                wordCount.remove(c);
+            } else{
+                wordCount.put(c,wordCount.get(c)-1);
+            }
+        } else{
+            throw new RuntimeException(String.format("%c not found in Map", c));
+        }
+    }
+
+    private boolean isValidSubStr(int k){
+        if(wordCount.size()<=k){
+            return true;
+        }
+        return false;
+    }
+
+}
