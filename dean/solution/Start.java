955941a1ac75e991593f59590d4864276dfb9116
root <yliu224@dons.usfca.edu>
2021-04-22 02:05:08 +0000

    AC 1422 hard

--- dean/solution/Start.java
+++ dean/solution/Start.java
@@ -1,9 +1,10 @@
-import bfs.lc1828_m.*;
+import bfs.lc1422_hh.*;
 
 public class Start {
     public static void main(String[] args){
         Solution s = new Solution();
 
-        System.out.println(s.lakeEscape(4,new int[][]{{1,1,1,1},{1,-1,-1,1},{-1,1,1,-1},{1,-1,-1,1}},2,1,2,2));
+        //System.out.println(s.lakeEscape(4,new int[][]{{1,1,1,1},{1,-1,-1,1},{-1,1,1,-1},{1,-1,-1,1}},2,1,2,2));
+        s.shortestPathLength(new int[][]{{1,2,3},{0},{0},{0}});
     }
 }
